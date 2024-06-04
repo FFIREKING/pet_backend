6054440fa7e643daaa9b68c8f840f81809a520d0
maksym <maksym@decisionlift.com>
2024-06-04 07:27:17 -0500

    Update

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def send_to_production(shop_id, order_id):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app_asgi, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
